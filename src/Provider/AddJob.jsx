ade94f1f4ff6c61b768f7d9bc5c613732359f172
Taralama <tarahero65@gmail.com>
2024-04-06 21:15:17 +0545

    add job and joblisting corrected

--- src/Provider/AddJob.jsx
+++ src/Provider/AddJob.jsx
@@ -1,9 +1,36 @@
 import { Box, Button, Icon, Input, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { SimpleGrid } from "@chakra-ui/react";
+import axios from "axios";
 const AddJob = () => {
+  const [values, setValues] = useState({
+    CompanyName: "",
+    Location: "",
+    Post: "",
+    Time: "",
+    Salary: "",
+    Description: "",
+    Date: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/addjob", {
+        ...values,
+      });
+      if (response.data.Status === "Success") {
+        alert("Job posted");
+      } else {
+        alert(response.data.Error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Box display={"flex"}>
@@ -37,123 +64,150 @@ const AddJob = () => {
           </Box>
         </Link>
       </Box>
-      <Box bgColor={""} h={"87vh"}>
-        <SimpleGrid
-          columns={1}
-          spacing={1}
-          mt={"5vh"}
-          ml={"18vw"}
-          position={"relative"}
-          top={"20px"}
-        >
-          <Text letterSpacing={2} fontFamily={"Times New roman"}>
-            Company Name:
-          </Text>
-          <Input
-            bgColor={"white"}
-            border={"1px solid"}
-            height="5vh"
-            width={"35vw"}
-            borderRadius={"10px"}
-          ></Input>
-          <Text letterSpacing={2} fontFamily={"Times New roman"}>
-            Location:
-          </Text>
-          <Input
-            bgColor={"white"}
-            border={"1px solid"}
-            height="5vh"
-            width={"35vw"}
-            borderRadius={"10px"}
-          ></Input>
-          <Text letterSpacing={2} fontFamily={"Times New roman"}>
-            Post:
-          </Text>
-          <Input
-            bgColor={"white"}
-            border={"1px solid"}
-            height="5vh"
-            width={"35vw"}
-            borderRadius={"10px"}
-          ></Input>
-          <Text letterSpacing={2} fontFamily={"Times New roman"}>
-            Time:
-          </Text>
-          <Input
-            bgColor={"white"}
-            border={"1px solid"}
-            height="5vh"
-            width={"35vw"}
-            borderRadius={"10px"}
-          ></Input>
-          <Text letterSpacing={2} fontFamily={"Times New roman"}>
-            Salary:
-          </Text>
-          <Input
-            bgColor={"white"}
-            border={"1px solid"}
-            height="5vh"
-            width={"35vw"}
-            borderRadius={"10px"}
-          ></Input>
-          <Text letterSpacing={2} fontFamily={"Times New roman"}>
-            Description:
-          </Text>
-          <Input
-            bgColor={"white"}
-            border={"1px solid"}
-            height="15vh"
-            width={"35vw"}            borderRadius={"10px"}
-          ></Input>
-        </SimpleGrid>
-        <Box
-          bgColor={"black"}
-          h={"92vh"}
-          w={"1px"}
-          position={"absolute"}
-          top={"5vh"}
-          left={"60vw"}
-        ></Box>
-        <Box position={"absolute"} top={"20vh"} right={"18vw"}>
-          <Text letterSpacing={2} fontFamily={"Times New roman"}>
-            Deadline:
-          </Text>
-          <Input
-            type="date"
-            bgColor={"white"}
-            border={"1px solid"}
-            height="40px"
-            borderRadius={"10px"}
-          ></Input>
-        </Box>
-        <Box position={"absolute"} top={"40vh"} right={"7.6vw"}>
-          <Text
-            letterSpacing={2}
-            fontFamily={"Times New roman"}
-            margin={"20px"}
+      <form onSubmit={handleSubmit}>
+        <Box bgColor={""} h={"87vh"}>
+          <SimpleGrid
+            columns={1}
+            spacing={1}
+            mt={"5vh"}
+            ml={"18vw"}
+            position={"relative"}
+            top={"20px"}
           >
-            Upload Company Logo:
-          </Text>
+            <Text letterSpacing={2} fontFamily={"Times New roman"}>
+              Company Name:
+            </Text>
+            <Input
+              bgColor={"white"}
+              border={"1px solid"}
+              height="5vh"
+              width={"35vw"}
+              borderRadius={"10px"}
+              onChange={(e) => {
+                setValues({ ...values, CompanyName: e.target.value });
+              }}
+            ></Input>
+            <Text letterSpacing={2} fontFamily={"Times New roman"}>
+              Location:
+            </Text>
+            <Input
+              bgColor={"white"}
+              border={"1px solid"}
+              height="5vh"
+              width={"35vw"}
+              borderRadius={"10px"}
+              onChange={(e) => {
+                setValues({ ...values, Location: e.target.value });
+              }}
+            ></Input>
+            <Text letterSpacing={2} fontFamily={"Times New roman"}>
+              Post:
+            </Text>
+            <Input
+              bgColor={"white"}
+              border={"1px solid"}
+              height="5vh"
+              width={"35vw"}
+              borderRadius={"10px"}
+              onChange={(e) => {
+                setValues({ ...values, Post: e.target.value });
+              }}
+            ></Input>
+            <Text letterSpacing={2} fontFamily={"Times New roman"}>
+              Time:
+            </Text>
+            <Input
+              bgColor={"white"}
+              border={"1px solid"}
+              height="5vh"
+              width={"35vw"}
+              borderRadius={"10px"}
+              onChange={(e) => {
+                setValues({ ...values, Time: e.target.value });
+              }}
+            ></Input>
+            <Text letterSpacing={2} fontFamily={"Times New roman"}>
+              Salary:
+            </Text>
+            <Input
+              bgColor={"white"}
+              border={"1px solid"}
+              height="5vh"
+              width={"35vw"}
+              borderRadius={"10px"}
+              onChange={(e)=>{
+                setValues({
+                  ...values,Salary: e.target.value
+                })
+              }}
+            ></Input>
+            <Text letterSpacing={2} fontFamily={"Times New roman"}>
+              Description:
+            </Text>
+            <Input
+              bgColor={"white"}
+              border={"1px solid"}
+              height="15vh"
+              width={"35vw"}
+              borderRadius={"10px"}
+              onChange={(e) => {
+                setValues({ ...values, Description: e.target.value });
+              }}
+            ></Input>
+          </SimpleGrid>
+          <Box
+            bgColor={"black"}
+            h={"92vh"}
+            w={"1px"}
+            position={"absolute"}
+            top={"5vh"}
+            left={"60vw"}
+          ></Box>
+          <Box position={"absolute"} top={"20vh"} right={"18vw"}>
+            <Text letterSpacing={2} fontFamily={"Times New roman"}>
+              Deadline:
+            </Text>
+            <Input
+              type="date"
+              bgColor={"white"}
+              border={"1px solid"}
+              height="40px"
+              borderRadius={"10px"}
+              onChange={(e) => {
+                setValues({ ...values, Date: e.target.value });
+              }}
+            ></Input>
+          </Box>
+          <Box position={"absolute"} top={"40vh"} right={"7.6vw"}>
+            <Text
+              letterSpacing={2}
+              fontFamily={"Times New roman"}
+              margin={"20px"}
+            >
+              Upload Company Logo:
+            </Text>
 
-          <Input
-            border={"none"}
-            type="file"
-            height="40px"
-            borderRadius={"10px"}
-          ></Input>
+            <Input
+              border={"none"}
+              type="file"
+              height="40px"
+              borderRadius={"10px"}
+            ></Input>
+          </Box>
+          <Button
+            position={"absolute"}
+            height={"6vh"}
+            border={"1px solid"}
+            fontFamily={"times new roman"}
+            top={"70vh"}
+            right={"20vw"}
+            letterSpacing={2}
+            type="submit"
+          >
+            Add Job
+          </Button>
         </Box>
-        <Button
-          position={"absolute"}
-          height={"6vh"}
-          border={"1px solid"}
-          fontFamily={"times new roman"}
-          top={"70vh"}
-          right={"20vw"}
-          letterSpacing={2}
-        >
-          Add Job
-        </Button>
-      </Box>
+      </form>
     </>
   );
 };
--- src/sub-pages/Recruitment.jsx
+++ src/sub-pages/Recruitment.jsx
@@ -1,115 +1,139 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import lines44 from "../assets/img/Line44.png";
-
+import { SimpleGrid } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { Box, Icon, Image, Input, Text } from "@chakra-ui/react";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import Apply from "../assets/img/Apply.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const Recruitment = () => {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:3001/recruitment");
+      console.log(response.data[0].CompanyName);
+
+      setData(response.data);
+    };
+    fetchData();
+  }, []);
+
+  console.log(data.length);
+
   return (
     <>
-      <Box border={"1px solid white "} h={"10vh"} marginLeft={"19%"}>
-        <Link to="/userprofile">
-          <Box
-            cursor={"pointer"}
-            display={"flex"}
-            position={"absolute"}
-            right={"40px"}
-            gap={"10px"}
-            marginTop={"28.8px"}
-            zIndex={"2"}
-            alignItems={"center"}
-          >
-            <Icon
-              as={IoPersonCircleSharp}
-              height={"30px"}
-              width={"30px"}
-              color={"white"}
-              cursor={"pointer"}
-            />
-            <Text fontWeight={600} color={"white"}>
-              Profile
-            </Text>
-          </Box>
-        </Link>
-        <Box display={"flex"} gap={"220px"} marginTop={"28px"}>
-          <form action="/search" method="get" backgroundColor={"#414141"}>
-            <Input
-              type="text"
-              id="search"
-              name="q"
-              placeholder="Search ..."
-              _placeholder={{
-                color: "#faf9f0",
-              }}
-              backgroundColor={"#414141"}
-              color={"white"}
-              borderRadius={"7px"}
-              height={"30px"}
-            />
-          </form>
-          <SearchIcon
-            height={"28px"}
-            marginLeft={"-200px"}
-            cursor={"pointer"}
-          />
-        </Box>
-      </Box>
-
-      <Text
-        marginLeft={"41rem"}
-        marginTop={"20px"}
-        fontSize={"20px"}
-        letterSpacing={2.5}
-      >
-        Job Listings
-      </Text>
-      <Image src={lines44} height={"10px"} marginLeft={"43%"} alt=""></Image>
+      {" "}
       <Box
-        border={"1px solid"}
-        borderRadius={"4px 4px 0px 4px"}
-        bgColor={"#faf9f0"}
-        zIndex={2}
-        height={"200px"}
-        w={"75%"}
-        marginLeft={"280px"}
+        // border={"1px solid green"}
+        position={"absolute"}
+        height={"100vh"}
+        right={"0"}
+        width={"85vw"}
       >
-        <Box border={"1px solid"} w={"20%"} h={"100%"}>
-          {" "}
-          Logo of the company goes here
+        <Box 
+        // border={"1px solid red"} 
+        height={"10vh"}>
+          <Link to="/userprofile">
+            <Box
+              cursor={"pointer"}
+              display={"flex"}
+              position={"absolute"}
+              right={"2vw"}
+              gap={".5vw"}
+              marginTop={"28.8px"}
+              zIndex={"2"}
+              alignItems={"center"}
+            >
+              <Icon
+                as={IoPersonCircleSharp}
+                height={"30px"}
+                width={"30px"}
+                color={"white"}
+                cursor={"pointer"}
+              />
+              <Text fontWeight={600} color={"white"}>
+                Profile
+              </Text>
+            </Box>
+          </Link>
         </Box>
-        <Box
-          border={"1px solid"}
-          position={"absolute"}
-          w={"55%"}
-          h={"20%"}
-          left={"550px"}
-          bottom={"125px"}
-        >
-          Display post, jobtype and salary in this section
-        </Box>
-        <Box
-          border={"1px solid"}
-          position={"absolute"}
-          w={"55%"}
-          h={"20%"}
-          left={"550px"}
-          bottom={"40px"}
-        >
-          Location, Deadline and qualifications
-        </Box>
-        <Link to="/application">
+        <Box 
+        // border={"1px solid red"}
+         height={"10vh"}>
+          <Text
+            display={"flex"}
+            justifyContent={"center"}
+            fontSize={"5vh"}
+            letterSpacing={2.5}
+          >
+            Job Listings
+          </Text>
           <Image
-            src={Apply}
+            src={lines44}
+            height={"10px"}
+            marginLeft={"43%"}
             alt=""
-            h={"6%"}
-            cursor={"pointer"}
-            marginLeft={"1047.9px"}
-            top={"332px"}
-            position={"fixed"}
-          />
-        </Link>
+          ></Image>
+        </Box>
+
+        <Box
+          height={"79.9vh"}
+          // border={"1px solid green"}
+          padding={"1vh 10vh 10vh 1vh"}
+        >
+          {/* this is the where list will be appeared */}
+          {data.map((newdata) => (
+            <Box
+              // border={"1px solid blue"}
+              bgColor={"#faf9f0"}
+              height={"15vh"}
+              mt={"1vh"}
+              borderRadius={"2vh"}
+              boxShadow={"1px 1px 10px black"}
+              display={"flex"}
+              flexDirection={"row"}
+              mb={"2vh"}
+            >
+              <Box height={"15vh"} border={"1px solid black"} flex={"1"}>
+                here will be picture
+              </Box>
+              <Box
+                height={"15vh"}
+                border={"1px solid black"}
+                flex={"4"}
+                letterSpacing={1}
+                fontFamily={"times new roman"}
+              >
+                <Text fontWeight={"bold"}>
+                  {/* Post */}
+                  {newdata.Post}
+                </Text>
+                <Text fontWeight={"bold"}>
+                  {/* companyname */}
+                  {newdata.CompanyName}
+                </Text>
+                <Text display={"inline-block"}>{newdata.Location}</Text>
+                <Text ml={"10vw"} display={"inline-block"}>
+                  Salary: {newdata.Salary}
+                </Text>
+                <Text>{newdata.Description}</Text>
+              </Box>
+              <Box>
+                <Link to="/application">
+                  <Image
+                    src={Apply}
+                    position={"relative"}
+                    right={"-7vh"}
+                    h={"8vh"}
+                    cursor={"pointer"}
+                  />
+                </Link>
+              </Box>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </>
   );
